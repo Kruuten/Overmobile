6a50c0ed9a1ed5ae7b279fa14ce30f1339b3226c
anton <anton.kruten@gmail.com>
2022-05-12 14:34:31 +0300

    init commit

---
+++ src/main/java/kruten/overmobile/OvermobileApplication.java
@@ -0,0 +1,13 @@
+package kruten.overmobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvermobileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OvermobileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kruten/overmobile/OvermobileApplicationTests.java
@@ -0,0 +1,13 @@
+package kruten.overmobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvermobileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
