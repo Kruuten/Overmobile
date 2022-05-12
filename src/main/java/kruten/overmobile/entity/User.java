ef4f58ff48f7798c1db5e00b38bda5d4b57f8293
anton <anton.kruten@gmail.com>
2022-05-12 21:52:09 +0300

    add User class

---
+++ src/main/java/kruten/overmobile/entity/User.java
@@ -0,0 +1,53 @@
+package kruten.overmobile.entity;
+
+import org.springframework.data.annotation.Id;
+
+
+public class User {
+    @Id
+    private String id;
+
+    private long userId;
+    private double sum;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public double getSum() {
+        return sum;
+    }
+
+    public void setSum(double sum) {
+        this.sum = sum;
+    }
+
+    public User() {
+    }
+
+    public User(long userId, double sum) {
+        this.userId = userId;
+        this.sum = sum;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", sum=" + sum +
+                '}';
+    }
+}
