85ce9da7432779ee481535ac2ed032c2812ff4cf
anton <anton.kruten@gmail.com>
2022-05-12 21:51:11 +0300

    added service logic

---
+++ src/main/java/kruten/overmobile/service/UserService.java
@@ -0,0 +1,21 @@
+package kruten.overmobile.service;
+
+import kruten.overmobile.entity.User;
+import kruten.overmobile.repository.UserRep;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRep userRep;
+
+    @Transactional
+    public User postUser(User user) {
+        User optional = userRep.findById(user.getId());
+        if (optional == null)
+            optional = userRep.save(user);
+        return optional;
+    }
+}
