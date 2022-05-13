6ded017550538d790cf5035d23023c85a65afcc5
anton <anton.kruten@gmail.com>
2022-05-13 12:16:55 +0300

    add error message class

---
+++ src/main/java/kruten/overmobile/error/ErrorMessage.java
@@ -0,0 +1,14 @@
+package kruten.overmobile.error;
+
+public class ErrorMessage {
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
