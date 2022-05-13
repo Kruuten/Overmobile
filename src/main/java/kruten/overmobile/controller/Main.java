de16d4205e1d11e19d465eb18b3c927407631ca6
anton <anton.kruten@gmail.com>
2022-05-13 16:04:41 +0300

    add response

--- src/main/java/kruten/overmobile/controller/Main.java
+++ src/main/java/kruten/overmobile/controller/Main.java
@@ -2,9 +2,9 @@ package kruten.overmobile.controller;
 
 
 import kruten.overmobile.entity.User;
+import kruten.overmobile.error.ResponseHandler;
 import kruten.overmobile.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +18,8 @@ public class Main {
 
     @PostMapping("/add")
     public ResponseEntity postUser(@Valid @RequestBody User user) {
-            return new ResponseEntity<>(userService.postUser(user), HttpStatus.OK);
+        userService.postUser(user);
+        return ResponseHandler.generateResponse(0);
     }
+
 }
