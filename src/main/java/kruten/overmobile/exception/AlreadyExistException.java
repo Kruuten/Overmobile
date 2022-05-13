13b916d827467fcee9ed99bf4db64cd0c285c509
anton <anton.kruten@gmail.com>
2022-05-13 12:16:36 +0300

    add custom exception

---
+++ src/main/java/kruten/overmobile/exception/AlreadyExistException.java
@@ -0,0 +1,7 @@
+package kruten.overmobile.exception;
+
+public class AlreadyExistException extends RuntimeException{
+    public AlreadyExistException(String message) {
+        super(message);
+    }
+}
