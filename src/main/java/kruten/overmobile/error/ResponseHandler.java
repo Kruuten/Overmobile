bfaf7e6531a62303e199da8877a9076b61c46288
anton <anton.kruten@gmail.com>
2022-05-13 16:05:18 +0300

    add response handler

---
+++ src/main/java/kruten/overmobile/error/ResponseHandler.java
@@ -0,0 +1,16 @@
+package kruten.overmobile.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResponseHandler {
+    public static ResponseEntity<Object> generateResponse(int number) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("status", number);
+
+        return new ResponseEntity<Object>(map, HttpStatus.OK);
+    }
+}
