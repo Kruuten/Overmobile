629b02e6ed8ce681a0a7df8f258ec5706073267e
anton <anton.kruten@gmail.com>
2022-05-14 16:20:59 +0300

    change logic

--- src/main/java/kruten/overmobile/exception/AlreadyExistsException.java
+++ src/main/java/kruten/overmobile/exception/AlreadyExistsException.java
@@ -1,12 +1,7 @@
 package kruten.overmobile.exception;
 
-import kruten.overmobile.entity.User;
-
 public class AlreadyExistsException extends RuntimeException{
     public AlreadyExistsException(String message) {
         super(message);
-    }
-
-    public AlreadyExistsException(User user) {
     }
 }
